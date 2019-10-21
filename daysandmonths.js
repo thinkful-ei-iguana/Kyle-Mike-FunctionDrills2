47d3e47146eb7f0b4d963294e91f926272c6d2fe
Kyle Jensen <kylenjensen19@gmail.com>
2019-10-21 15:55:08 -0700

    Made the days and month file

---
+++ daysandmonths.js
@@ -0,0 +1,19 @@
+function daysAndMonths(month, ){
+    var days;
+    if (month !== 'January', 'February', 'March', 'April', 'May', 'June', 'July'
+    switch(month){
+       case 'April':
+        case 'June':
+        case 'September':
+        case 'November':
+        days = `${month} has 30 days.`;
+        case 'February':
+            if ()
+        break;
+        default :
+        days = `${month} has 31 days`;
+        }
+        return days;
+        
+   }
+console.log(daysAndMonths('kyle'));
