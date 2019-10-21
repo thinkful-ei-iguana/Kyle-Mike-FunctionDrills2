ab72486ad41b9082eeffa5925ee6f3b2dd03a6c7
Michael Weedman <michael.weedman@gmail.com>
2019-10-21 16:52:03 -0700

    Update daysandmonths.js

--- daysandmonths.js
+++ daysandmonths.js
@@ -1,19 +1,27 @@
-function daysAndMonths(month, ){
-    var days;
-    if (month !== 'January', 'February', 'March', 'April', 'May', 'June', 'July'
+function daysAndMonths(month, leapYear){
+    let days;
     switch(month){
-       case 'April':
+        case 'April':
         case 'June':
         case 'September':
         case 'November':
-        days = `${month} has 30 days.`;
+            days = `${month} has 30 days.`;
+            break;
         case 'February':
-            if ()
-        break;
-        default :
-        days = `${month} has 31 days`;
+            days = `${month} has ${leapYear ? 29:28} days.`;
+            break;
+        case 'January':
+        case 'March':
+        case 'May':
+        case 'July':
+        case 'August':
+        case 'October':
+        case 'December':
+            days = `${month} has 31 days`;
+            break;
+        default:
+            throw new Error ('Must provide a valid month.');
         }
         return days;
-        
    }
-console.log(daysAndMonths('kyle'));
+console.log(daysAndMonths('February', false));
